f2bfcb1d20b75c9f95838d912be8872bcebad456
remi-dev180 <defforemi10@gmail.com>
2024-12-08 05:57:08 +0100

    start

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <header>
+    <img class="logo1" src="img/logo.png" alt="">
+    <div class="h-center">
+      <span>Secure checkout</span>
+      <img src="img/chevron.png" alt="">
+    </div>
+    <div class="h-end">
+      <img src="img/cart.png" alt="">
+      <span>Cart</span>
+    </div>
+  </header>
+
+</body>
+</html>
